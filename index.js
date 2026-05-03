19a59ee97eea7fdfe4e830332cc399c0a38f2bc8
Sreymomcloud1 <sreymomc527@gmail.com>
2026-05-03 14:46:14 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("<h1>i think it's working...</h1>")
+})
+app.post('/', (req, res) => {
+  res.send("Received!")
+})
+
+app.listen(5000, () =>
+  console.log('EXPRESS Server Started at Port No: 5000'));
