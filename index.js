1b83af7362321710c88356524caba8a165881024
Sreymomcloud1 <sreymomc527@gmail.com>
2026-05-03 18:20:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
 
+// FIX: Disables the 'X-Powered-By: Express' header to prevent fingerprinting
+app.disable('x-powered-by');
+
 app.get('/', (req, res) => {
   res.send("<h1>creatingconflict branch version</h1>")
-})
+});
+
 app.post('/', (req, res) => {
   res.send("Received!")
-})
+});
 
-app.listen(5000, () =>
-  console.log('EXPRESS Server Started at Port No: 5000'));
+// Complete the listen function
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
