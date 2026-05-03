8b160b202eab971915bef1d24c275ce098237287
Sreymomcloud1 <sreymomc527@gmail.com>
2026-05-03 20:03:32 +0700

    Refactor index.js to simplify server setup

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express');
+// IMPORT Express Server
+const express = require('express'); 
 const app = express();
 
-// FIX: Disables the 'X-Powered-By: Express' header to prevent fingerprinting
-app.disable('x-powered-by');
+app.get('/', (req, res)=> {
+    res.send("<h1>i think it's working...</h1>")
+})
+app.post('/', (req, res)=> {
+    res.send("Received!")
+})
 
-app.get('/', (req, res) => {
-  res.send("<h1>creatingconflict branch version</h1>")
-});
-
-app.post('/', (req, res) => {
-  res.send("Received!")
-});
-
-// Complete the listen function
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(5000, () =>
+console.log('EXPRESS Server Started at Port No: 5000'));
